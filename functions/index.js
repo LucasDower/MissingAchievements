e4bd1318fbdb5fd61249f05746680f06cd07818e
Lucas Dower <43851835+LucasDower@users.noreply.github.com>
2020-03-14 11:05:48 +0000

    Refactor and cleanup

--- functions/index.js
+++ functions/index.js
@@ -4,52 +4,56 @@ const api_key = functions.config().steam.key;
 const u_id_re = new RegExp('^([0-9]).{16}$');
 const g_id_re = new RegExp('^[0-9]{1,}$');
 
+function testParameter(request, key, regex) {
+    if (!Object.keys(request.query).includes(key)) {
+        return false;
+    }
+    let value = request.query[key];
+    if (!regex.test(value)) {
+        return false;
+    }
+    return value;
+}
+
+async function makeRequest(request) {
+    await fetch(request)
+        .then(res => res.json())
+        .catch(err => {
+            console.error(err);
+            return [];
+        })
+        .then(json => {
+            return json;
+        })
+        .catch(err => {
+            console.error(err);
+            return [];
+        });
+}
 
 exports.getOwnedGames = functions.https.onRequest(async (request, response) => {
 
-    // Ensure the 'id' parameter has been given.
-    if (!Object.keys(request.query).includes('id')) {
-        console.log('Missing id parameter');
-        response.status(400).send("Missing 'id' parameter");
-        return;
-    }
-
-    const user_id = request.query.id;
-    console.info(user_id);
-
-    // Ensure the provided id is valid.
-    if (!u_id_re.test(user_id)) {
-        console.log('Invalid id parameter');
-        response.status(400).send("Invalid 'id' parameter");
+    // Ensure the 'u_id' parameter has been given and valid.
+    const u_id = testParameter(request, 'u_id', u_id_re);
+    if (!u_id) {
+        console.log('Invalid u_id parameter');
+        response.status(400).send("Invalid 'u_id' parameter");
         return;
     }
 
     // Get details of the games the user owns.
-    let steam_request = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${api_key}&steamid=${user_id}&include_appinfo=true`;
+    const steam_request = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${api_key}&steamid=${user_id}&include_appinfo=true`;
 
-    let steam_response = null;
-    await fetch(steam_request)
-        .then(res => res.json())
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from api.steampowered.com.');
-            return;
-        })
-        .then(json => {
-            steam_response = json;
-            return;
-        })
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from api.steampowered.com.');
-            return;
-        });
-
-    if (steam_response === null) {
-        console.log("Null route entered");
+    // Make request.
+    let steam_response = await makeRequest(steam_request);
+    if (steam_response.length === 0) {
+        const error = "Error when requesting IPlayerService/GetOwnedGames";
+        console.error(error);
+        response.status(500).send(error);
         return;
     }
 
+    // Parse response.
     const ownedGames = steam_response.response.games
         .filter(game => game.playtime_forever > 0)
         .map(game => { return { 'appid': game.appid, 'name': game.name }; })
@@ -63,6 +67,7 @@ exports.getOwnedGames = functions.https.onRequest(async (request, response) => {
             return 0;
         });
 
+    // Send response.
     response.setHeader('Content-Type', 'application/json');
     response.send(ownedGames);
     return;
@@ -72,119 +77,99 @@ exports.getOwnedGames = functions.https.onRequest(async (request, response) => {
 
 exports.getMissingAchievements = functions.https.onRequest(async (request, response) => {
 
-    // Ensure the 'u_id' and 'g_id' has been given.
-    const givenFields = Object.keys(request.query);
-    if (!givenFields.includes('u_id') || !givenFields.includes('g_id')) {
-        console.log("Missing 'u_id' and/or 'g_id' parameter");
-        response.status(400).send("Missing 'u_id' and/or 'g_id' parameter");
+    // Ensure the 'u_id' and 'g_id' has been given and valid.
+    const u_id = testParameter(request, 'u_id', u_id_re);
+    const g_id = testParameter(request, 'g_id', g_id_re);
+    if (!u_id || !g_id) {
+        console.log("Invalid 'u_id' and/or 'g_id' parameter");
+        response.status(400).send("Invalid 'u_id' and/or 'g_id' parameter");
         return;
     }
 
-    const u_id = request.query.u_id;
-    const g_id = request.query.g_id;
 
-    // Ensure the provided u_id is valid.
-    if (!u_id_re.test(u_id)) {
-        console.log('Invalid id parameter');
-        response.status(400).send("Invalid 'u_id' parameter");
-        return;
-    }
 
-    // Ensure the provided g_id is valid.
-    if (!g_id_re.test(g_id)) {
-        console.log("'Invalid 'g_id' parameter");
-        response.status(400).send("Invalid 'g_id' parameter");
-        return;
-    }
-
-    // Get details of the games the user owns.
+    // Get locked achievements.
     let steam_request = `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${g_id}&key=${api_key}&steamid=${u_id}`;
 
-    let steam_response = null;
-    await fetch(steam_request)
-        .then(res => res.json())
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from GetPlayerAchievements');
-            return;
-        })
-        .then(json => {
-            steam_response = json;
-            return;
-        })
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from GetPlayerAchievements');
-            return;
-        });
-
-    const user_locked = (steam_response.playerstats.achievements || [])
-        .filter(x => x.achieved === 0)
-        .map(x => x.apiname);
-
-    // ---------------------------------------
-
-    steam_request = `http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${g_id}`;
-
-    steam_response = null;
-    await fetch(steam_request)
-        .then(res => res.json())
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from GetGlobalAchievementPercentagesForApp');
-            return;
-        })
-        .then(json => {
-            steam_response = json;
-            return;
-        })
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from GetGlobalAchievementPercentagesForApp');
-            return;
-        });
-
-    try {
-    const global_achievements = steam_response.achievementpercentages.achievements
-        .filter(x => x.percent > 0);
-    } catch (e) {
-        console.info(err);
-            response.status(503).send('Error getting request from GetGlobalAchievementPercentagesForApp');
-            return;
+    // Make request.
+    let steam_response = await makeRequest(steam_request);
+    if (steam_response.length === 0) {
+        const error = "Error when requesting ISteamUserStats/GetPlayerAchievements";
+        console.error(error);
+        response.status(500).send(error);
+        return;
     }
 
-    // ---------------------------------------
+    // Parse response.
+    var user_locked;
+    try {
+        user_locked = steam_response.playerstats.achievements
+            .filter(x => x.achieved === 0)
+            .map(x => x.apiname);
+    } catch (e) {
+        console.info(err);
+        response.status(503).send('Error getting request from GetGlobalAchievementPercentagesForApp');
+        return;
+    }
 
+
+    // Get achievement unlock percentages.
+    steam_request = `http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=${g_id}`;
+
+    // Make request.
+    steam_response = await makeRequest(steam_request);
+    if (steam_response.length === 0) {
+        const error = "Error when requesting ISteamUserStats/GetGlobalAchievementPercentagesForApp";
+        console.error(error);
+        response.status(500).send(error);
+        return;
+    }
+
+    // Parse response.
+    let global_achievements;
+    try {
+        global_achievements = steam_response.achievementpercentages.achievements
+            .filter(x => x.percent > 0);
+    } catch (e) {
+        console.info(err);
+        response.status(503).send('Error when requesting ISteamUserStats/GetGlobalAchievementPercentagesForApp');
+        return;
+    }
+
+
+
+    // Get achievement details.
     steam_request = `http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${api_key}&appid=${g_id}`;
 
-    steam_response = null;
-    await fetch(steam_request)
-        .then(res => res.json())
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from GetSchemaForGame');
-            return;
-        })
-        .then(json => {
-            steam_response = json;
-            return;
-        })
-        .catch(err => {
-            console.info(err);
-            response.status(503).send('Error getting request from GetSchemaForGame');
-            return;
-        });
+    // Make request.
+    steam_response = await makeRequest(steam_request);
+    if (steam_response.length === 0) {
+        const error = "Error when requesting ISteamUserStats/GetSchemaForGame";
+        console.error(error);
+        response.status(500).send(error);
+        return;
+    }
 
-    const achievement_schemas = steam_response.game.availableGameStats.achievements
-        .filter(x => user_locked.includes(x.name))
-        .map(x => {
-            return {
-                'name': x.name,
-                'display_name': x.displayName,
-                'desc': x.description,
-                'icon': x.icon
-            };
-        });
+
+
+    // Parse response.
+    let achievement_schemas;
+    try {
+        achievement_schemas = steam_response.game.availableGameStats.achievements
+            .filter(x => user_locked.includes(x.name))
+            .map(x => {
+                return {
+                    'name': x.name,
+                    'display_name': x.displayName,
+                    'desc': x.description,
+                    'icon': x.icon
+                };
+            });
+    } catch (e) {
+        console.info(err);
+        response.status(503).send('Error when requesting ISteamUserStats/GetSchemaForGame');
+        return;
+    }
 
 
     let output = [];
@@ -217,7 +202,7 @@ exports.getMissingAchievements = functions.https.onRequest(async (request, respo
 
     }
 
-    
+
     output = output.sort((a, b) => {
         if (a.percent > b.percent) {
             return -1;
@@ -227,7 +212,7 @@ exports.getMissingAchievements = functions.https.onRequest(async (request, respo
         }
         return 0;
     });
-    
+
 
     response.setHeader('Content-Type', 'application/json');
     response.send(output);
